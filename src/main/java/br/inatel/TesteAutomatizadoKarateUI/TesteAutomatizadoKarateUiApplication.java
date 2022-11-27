7709e41fbaa2b8ee2801bf9c32ff1480f9a97823
wilmar vitor fonseca <wilmarfonseca@gec.inatel.br>
2022-11-27 15:55:45 -0300

    first commit

---
+++ src/main/java/br/inatel/TesteAutomatizadoKarateUI/TesteAutomatizadoKarateUiApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.TesteAutomatizadoKarateUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAutomatizadoKarateUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAutomatizadoKarateUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/inatel/TesteAutomatizadoKarateUI/TesteAutomatizadoKarateUiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.TesteAutomatizadoKarateUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAutomatizadoKarateUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
